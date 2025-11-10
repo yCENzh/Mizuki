5de4a99d73e6a0f1e7ce5da035201d8b2d16ead7
Hill <128226888+Hill-1024@users.noreply.github.com>
2025-11-10 11:51:47 +0800

    Define SITE_TIMEZONE for website timezone configuration
    Add SITE_TIMEZONE constant to configure website timezone.

--- src/config.ts
+++ src/config.ts
@@ -18,11 +18,13 @@ import { LinkPreset } from "./types/config";
 
 // 定义站点语言
 const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
-
+const SITE_TIMEZONE =8;//设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "Mizuki",
 	subtitle: "One demo website",
 
+	timeZone: SITE_TIMEZONE,
+	
 	lang: SITE_LANG,
 
 	themeColor: {
