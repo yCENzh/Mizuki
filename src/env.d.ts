e2d9ceebd95834e0e5b02b0ec3febc15a2bea965
城倉奏 <admin@thdog.moe>
2025-11-13 18:12:09 +0800

    Secures AES encryption with hashed password-derived key

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,8 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-  readonly UMAMI_API_KEY: string;
+  readonly UMAMI_API_KEY?: string;
+  readonly BCRYPT_SALT_ROUNDS?: string;
 }
 
 interface ImportMeta {
