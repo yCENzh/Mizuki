06de323c0d35a6ba7df744790b86034f11f9af27
松坂有希 <becky.balde@gmail.com>
2025-11-15 20:17:18 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -47,7 +47,7 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 顶栏标题文本
-		text: "MizukiGUI",
+		text: "MizukiUI",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 	},
