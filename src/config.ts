58345b23e974d7d05f07d9133495fbab76b19898
城倉奏 <admin@thdog.moe>
2025-11-17 21:50:58 +0800

    Adds support for custom HTML footer content

--- src/config.ts
+++ src/config.ts
@@ -382,9 +382,12 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 export const footerConfig: FooterConfig = {
 	enable: false, // 是否启用Footer HTML注入功能
+	customHtml: "", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
+	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
+	// FooterConfig.html 可能会在未来的某个版本弃用
 };
 
-// 直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 
 /**
  * 侧边栏布局配置
