5d992780d75be361565313cf53bee2b1e40865e2
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2026-01-21 15:27:05 +0900

    Update Japanese translation for 'about' key

--- src/i18n/languages/ja.ts
+++ src/i18n/languages/ja.ts
@@ -3,7 +3,7 @@ import type { Translation } from "../translation";
 
 export const ja: Translation = {
 	[Key.home]: "ホーム",
-	[Key.about]: "このブログについて",
+	[Key.about]: "情報",
 	[Key.archive]: "アーカイブ",
 	[Key.search]: "検索",
 	[Key.other]: "その他",
