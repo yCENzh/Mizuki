0caa8375011d86c86a4eec5c52754833bf7ff30a
matsuzaka-yuki <becky.balde@gmail.com>
2026-03-18 20:40:34 +0800

    refactor(posts): migrate post components to features/posts
    - Move PostCard, PostMeta, PostPage to features/posts/
    - Update imports in [...page].astro, [permalink].astro, [...slug].astro
    - Add types.ts and index.ts for posts module

---
+++ src/components/features/posts/index.ts
@@ -0,0 +1,4 @@
+export { default as PostCard } from "./PostCard.astro";
+export { default as PostMeta } from "./PostMeta.astro";
+export { default as PostPage } from "./PostPage.astro";
+export * from "./types";
