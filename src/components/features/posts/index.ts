c721e92fbc4c429f021458aea3ac07cb521421d4
matsuzaka-yuki <becky.balde@gmail.com>
2026-03-21 00:49:24 +0800

    feat(posts): 新增文章组件并重构页面以提升可维护性

--- src/components/features/posts/index.ts
+++ src/components/features/posts/index.ts
@@ -1,4 +1,7 @@
+export { default as LastModified } from "./LastModified.astro";
 export { default as PostCard } from "./PostCard.astro";
 export { default as PostMeta } from "./PostMeta.astro";
+export { default as PostNavigation } from "./PostNavigation.astro";
 export { default as PostPage } from "./PostPage.astro";
+export { default as ShareCard } from "./ShareCard.astro";
 export * from "./types";
